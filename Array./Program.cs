028187dad421e242c02d245891419affc3b1a560
Nicholasjohnsonstudent <112421143+Nicholasjohnsonstudent@users.noreply.github.com>
2023-01-24 18:49:15 +0000

    did sortAscending

--- Array./Program.cs
+++ Array./Program.cs
@@ -66,4 +66,25 @@
         }
         return total;
     }
+    public static int SortAscending(int[] nums)
+    {
+        int temp;
+        int[] newArray = new int[nums.Length];
+        for (int i = 0; i < nums.Length - 1 ; i++)
+        {
+            for (int j = i + 1;j< nums.Length; j++)
+            {
+                if (nums[i] < nums[j])
+                {
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        foreach (int n in nums)
+        {
+            Console.Write($"{n} ");
+        }
+    }
 }
