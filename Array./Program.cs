b97813c8e4c73ba4371dccd80f5a75b7a6c8d27d
Nicholasjohnsonstudent <112421143+Nicholasjohnsonstudent@users.noreply.github.com>
2023-01-24 19:31:36 +0000

    started Reverse method

--- Array./Program.cs
+++ Array./Program.cs
@@ -66,10 +66,46 @@
         }
         return total;
     }
-    public static int SortAscending(int[] nums)
+    public static int[] SortAscending(int[] nums)
     {
         int temp;
-        int[] newArray = new int[nums.Length];
+        // int[] newArray = new int[nums.Length];
+        for (int i = 0; i < nums.Length - 1 ; i++)
+        {
+            for (int j = i + 1;j< nums.Length; j++)
+            {
+                if (nums[i] > nums[j])
+                {
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+    public static double[] SortAscending(double[] nums)
+    {
+        double temp;
+        // int[] newArray = new int[nums.Length];
+        for (int i = 0; i < nums.Length - 1 ; i++)
+        {
+            for (int j = i + 1;j< nums.Length; j++)
+            {
+                if (nums[i] > nums[j])
+                {
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+    public static int[] SortDecending(int[] nums)
+    {
+        int temp;
+        // int[] newArray = new int[nums.Length];
         for (int i = 0; i < nums.Length - 1 ; i++)
         {
             for (int j = i + 1;j< nums.Length; j++)
@@ -82,9 +118,64 @@
                 }
             }
         }
+        return nums;
+    }
+    public static double[] SortDecending(double[] nums)
+    {
+        double temp;
+        // int[] newArray = new int[nums.Length];
+        for (int i = 0; i < nums.Length - 1 ; i++)
+        {
+            for (int j = i + 1;j< nums.Length; j++)
+            {
+                if (nums[i] < nums[j])
+                {
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+    public static int Sum(int[] nums)
+    {
+        int total = 0;
         foreach (int n in nums)
         {
-            Console.Write($"{n} ");
+            total += n;
         }
+        return total;
+    }
+    public static double Sum(double[] nums)
+    {
+        double total = 0;
+        foreach (double n in nums)
+        {
+            total += n;
+        }
+        return total;
+    }
+    public static int Product(int[] nums)
+    {
+        int total = 1;
+        foreach (int n in nums)
+        {
+            total *= n;
+        }
+        return total;
+    }
+    public static double Product(double[] nums)
+    {
+        double total = 1;
+        foreach (double n in nums)
+        {
+            total *= n;
+        }
+        return total;
+    }
+    public static int Reverse(int[] nums)
+    {
+        
     }
 }
