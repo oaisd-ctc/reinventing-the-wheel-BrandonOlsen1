c8e0814ee54c91173a478df51bc85125dfa63b3f
Nicholasjohnsonstudent <112421143+Nicholasjohnsonstudent@users.noreply.github.com>
2023-01-24 18:16:55 +0000

    started ArrayUtils

---
+++ Array./Program.cs
@@ -0,0 +1,69 @@
+﻿public static class ArrayUtils
+{
+    public static int Average(int[] nums)
+    {
+        int total = 0;
+        foreach (int n in nums)
+        {
+            total += n;
+        }
+        return total / nums.Length;
+    }
+    public static double Average(double[] nums)
+    {
+        double total = 0;
+        foreach (double n in nums)
+        {
+            total += n;
+        }
+        return total / nums.Length;
+    }
+    public static int Max(int[] nums)
+    {
+        int total = 0;
+        foreach (int n in nums)
+        {
+            if (n > total)
+            {
+                total = n;
+            }
+        }
+        return total;
+    }
+    public static double Max(double[] nums)
+    {
+        double total = 0;
+        foreach (double n in nums)
+        {
+            if (n > total)
+            {
+                total = n;
+            }
+        }
+        return total;
+    }
+    public static int Min(int[] nums)
+    {
+        int total = 0;
+        foreach (int n in nums)
+        {
+            if (n < total)
+            {
+                total = n;
+            }
+        }
+        return total;
+    }
+    public static double Min(double[] nums)
+    {
+        double total = 0;
+        foreach (double n in nums)
+        {
+            if (n < total)
+            {
+                total = n;
+            }
+        }
+        return total;
+    }
+}
