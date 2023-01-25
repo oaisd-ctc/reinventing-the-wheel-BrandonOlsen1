81bef90def139c9ecff424244e9f7c0edcb4f330
Dominic Felicelli <112422095+Dominic-Felicelli-student@users.noreply.github.com>
2023-01-25 18:51:13 +0000

    Wrote the ToUpper Function

--- Dominic/StringUtils.cs
+++ Dominic/StringUtils.cs
@@ -4,11 +4,126 @@
     {
         public static int Length(string str)
         {
-            for ( int i = 0 ; str[i] != '' ; i++ )
-            {
-                return i;
-            }
-            return 0;
+            return str.Length;
         }
+        public static string ToUpper(string str)
+        {
+            string newStr = "";
+            int len = Length(str);
+            for ( int i = 0 ; i < len ; i++ )
+            {
+                char c = str[i];
+                if ( c == 'a' )
+                {
+                    newStr += 'A';
+                }
+                else if (c == 'b' )
+                {
+                    newStr += 'B';
+                }
+                else if (c == 'c' )
+                {
+                    newStr += 'C';
+                }
+                else if (c == 'd' )
+                {
+                    newStr += 'D';
+                }
+                else if (c =='e' )
+                {
+                    newStr += 'E';
+                }
+                else if (c == 'f' )
+                {
+                    newStr += 'F';
+                }
+                else if (c == 'g')
+                {
+                    newStr += 'G';
+                }
+                else if (c == 'h')
+                {
+                    newStr += 'H';
+                }
+                else if (c == 'i')
+                {
+                    newStr += 'I';
+                }
+                else if (c == 'j')
+                {
+                    newStr += 'J';
+                }
+                else if (c == 'k')
+                {
+                    newStr += 'K';
+                }
+                else if (c == 'l')
+                {
+                    newStr += 'L';
+                }
+                else if (c == 'm')
+                {
+                    newStr += 'M';
+                }
+                else if (c == 'n')
+                {
+                    newStr += 'N';
+                }
+                else if (c == 'o')
+                {
+                    newStr += 'O';
+                }
+                else if (c == 'p')
+                {
+                    newStr += 'P';
+                }
+                else if (c == 'q')
+                {
+                    newStr += 'q';
+                }
+                else if (c == 'r')
+                {
+                     newStr += 'R';
+                }
+                else if (c == 's')
+                {
+                    newStr += 'S';
+                }
+                else if (c == 't')
+                {
+                    newStr += 'T';
+                }
+                else if (c == 'u')
+                {
+                    newStr += 'U';
+                }
+                else if (c == 'v')
+                {
+                    newStr += 'V';
+                }
+                else if (c == 'w')
+                {
+                    newStr += 'W';
+                }
+                else if (c == 'x')
+                {
+                    newStr += 'X';
+                } 
+                else if (c == 'y')
+                {
+                    newStr += 'Y';
+                }
+                else if (c == 'z')
+                {
+                    newStr+= 'Z';
+                }
+                else 
+                {
+                    newStr += c;
+                }
+            }
+            return str;
+        }
+        
     }
 }
