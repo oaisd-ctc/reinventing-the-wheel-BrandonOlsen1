3c0d50bb78e648781aa693fd9c4adabeee73ce28
Dominic Felicelli <112422095+Dominic-Felicelli-student@users.noreply.github.com>
2023-01-25 19:24:01 +0000

    Wrote ToLower Function

--- Dominic/StringUtils.cs
+++ Dominic/StringUtils.cs
@@ -28,6 +28,26 @@
             }
             return str;
         }
-        
+        public static string ToLower(string str)
+        {
+            string newStr = "";
+            int len = Length(str);
+            char[] ltr = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            char[] utr = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            foreach (char cr in str)
+            {
+                foreach (char uc in utr)
+                {
+                    if (cr == uc )
+                    {
+                        newStr += ltr[uc]; 
+                    }
+                    else{
+                        newStr += str[cr];
+                    }
+                }
+            }
+            return newStr;
+        }
     }
 }
