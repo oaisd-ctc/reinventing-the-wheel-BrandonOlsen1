dea6a8605542d90511bce1cfae6a7452e49ac0a3
Dominic Felicelli <112422095+Dominic-Felicelli-student@users.noreply.github.com>
2023-01-26 19:23:40 +0000

    Refreshed ToUpper

--- Dominic/StringUtils.cs
+++ Dominic/StringUtils.cs
@@ -8,11 +8,12 @@
         }
         public static string ToUpper(string str)
         {
-            string newStr = new string('f*');
+            string newStr = "";
             char[] ltr = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
             char[] utr = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
             foreach (char cr in str)
             {
+                
                 foreach ( char lc in ltr )
                 {
                     if ( lc == cr )
